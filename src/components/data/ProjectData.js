7aaf425e4014007ea97377f4455855d615959105
Your Name <metayerkinson@gmail.com>
2023-08-08 23:28:19 -0400

    updated peojectData.js

--- src/components/data/ProjectData.js
+++ src/components/data/ProjectData.js
@@ -7,37 +7,37 @@ import imgWeatherDashboard from "../../images/weather-dashboard.jpg";
 import imgNoteTaker from "../../images/note-taker.jpg";
 import imgReadmeGenerator from "../../images/readme-generator.jpg";
 import imgWorkDayScheduler from "../../images/work-day-scheduler.jpg";
-import imgPasswordGenerator from "../../images/password-generator.jpg";
+
 import imgBookSearchEngine from "../../images/book-search-engine.jpg";
 // Export and array of project objects
 export const projects = [
   {
     title: "Book Search Engine",
-    url: "https://dpk5e7-book-search-engine.herokuapp.com/",
+    url: "https://github.com/metayerkinson/mern-book-search-engine",
     image: imgBookSearchEngine,
     description:
       "Challenge #21 for the University of Central Florida Coding Bootcamp 2023. The purpose of this challenge is to build a MERN stack application.",
     technologies: "MongoDB, Express, React, Node, Apollo Server, & GraphQL",
-    GitHubRepo: "https://github.com/dpk5e7/book-search-engine/",
+    GitHubRepo: "https://github.com/metayerkinson/mern-book-search-engine",
   },
   {
     title: "Social Network API",
-    url: "https://github.com/metayerkinson/social-network-api",
+    url: "https://github.com/metayerkinson/nosql-social-network-backend",
     image: imgSocialNetworkAPI,
     description:
       "Challenge #18 for the University of Central Florida Coding Bootcamp 2023. The purpose of this challenge is to understand the unstructured nature of NoSQL by building an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
     technologies: "Node.js, Express.js, MongoDB, & Mongoose",
-    GitHubRepo: "https://github.com/metayerkinson/nosql-social-network-app",
+    GitHubRepo: "https://github.com/metayerkinson/nosql-social-network-backend",
   },
 
   {
     title: "e-Commerce Back End",
-    url: "https://github.com/metayerkinson/e-commerce-back-end",
+    url: "https://github.com/metayerkinson/e-commerce-backend",
     image: imgECommerceBackend,
     description:
       "Challenge #13 for the University of Central Florida Coding Bootcamp 2023. The purpose of this challenge is to build a back end for a generic e-commerce site using one-to-many and many-to-many database relationships.",
     technologies: "Node.js, Express.js, MySQL, & Sequelize",
-    GitHubRepo: "https://github.com/metayerkinson/ecommerce-backend",
+    GitHubRepo: "https://github.com/metayerkinson/e-commerce-backend",
   },
   {
     title: "Employee Tracker",
@@ -68,12 +68,12 @@ export const projects = [
   },
   {
     title: "Weather Dashboard",
-    url: "https://github.com/metayerkinson/weather-dashboard",
+    url: "https://github.com/metayerkinson/myweatherdashboard",
     image: imgWeatherDashboard,
     description:
       "Challenge #6 for the University of Central Florida Coding Bootcamp 2023. The purpose of this challenge is to build a weather dashboard that calls the OpenWeather API.",
     technologies: "HTML, CSS, Moment.js, Bootstrap 5.2, & OpenWeather API",
-    GitHubRepo: "https://github.com/metayerkinson/weather-dashboard",
+    GitHubRepo: "https://github.com/metayerkinson/myweatherdashboard",
   },
   {
     title: "Work Day Scheduler",
@@ -83,15 +83,5 @@ export const projects = [
       "Challenge #5 for the University of Central Florida Coding Bootcamp 2023. The purpose of this challenge is to build a work day scheduler.",
     technologies: "HTML, CSS, jQuery, Moment.js & Bootstrap 5.2",
     GitHubRepo: "https://github.com/metayerkinson/work-day-scheduler",
-  },
-
-  {
-    title: "Password Generator",
-    url: "https://github.com/metayerkinson/password-generator",
-    image: imgPasswordGenerator,
-    description:
-      "Challenge #3 for the University of Central Florida Coding Bootcamp 2023. The purpose of this challenge is to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected.",
-    technologies: "Javascript",
-    GitHubRepo: "https://github.com/metayerkinson/password-generator",
   },
 ];
